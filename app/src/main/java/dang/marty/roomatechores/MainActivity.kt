c740ce8cf2d37407b2cbd7cd002bcc3171090231
Marty Dang <marty.dang@bottomline.com>
2020-01-01 16:07:27 -0500

    Added in bottom navigation using the navigation componenents

--- app/src/main/java/dang/marty/roomatechores/MainActivity.kt
+++ app/src/main/java/dang/marty/roomatechores/MainActivity.kt
@@ -2,11 +2,25 @@ package dang.marty.roomatechores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(bottom_navigation, navController)
+    }
+
 }
---
+++ app/src/main/java/dang/marty/roomatechores/viewModels/MyChoresViewModel.kt
@@ -0,0 +1,8 @@
+package dang.marty.roomatechores.viewModels
+
+/**
+ *   Created by Marty Dang on 2019-12-31
+ *   Copyright @ 2019 Dang, Marty. All rights reserved.
+ */
+class MyChoresViewModel {
+}
